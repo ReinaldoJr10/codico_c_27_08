871ec96c57b46e9cec8bdfaba61a07be09578a1d
Reinaldo Coutinho <reinaldocsj01@gmail.com>
2019-08-27 21:39:05 -0300

    adicinei um codigo para identificar numero primo

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+int primo(int x){
+	int n=0;
+	for(int i=1;i!=x+1;i++){
+		if(x%i==0)
+			n++;
+	}
+	printf("\n%d",n);
+	if(n==2)
+		return 1;
+	else
+		return 0;}
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+	int x,y;
+	printf("Digite um numero primo:\n");
+    	scanf("%d",&x);
+    	y=primo(x);
+	if(y)
+		printf("o numero eh primo!!!");
+	else
+		printf("o numero nao eh primo.");
+    	return 0;
 }
